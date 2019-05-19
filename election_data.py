2a3b63c41760e20ebcfa40f5184d26cea1bce918
Helge <helge.krueger@tngtech.com>
2019-05-19 16:46:48 +0200

    Make resistent enough to load german data

--- election_data.py
+++ election_data.py
@@ -7,18 +7,29 @@ from loader import load_url
 class ElectionData():
     def __init__(self, url, parties=['CDU', 'SPD', 'GRÜNE','FDP', 'LINKE' , 'AfD'],
             start_date=pd.to_datetime('2018-01-01'),
-            time_period='30d'):
+            time_period='30d',
+            date_column='Datum'):
 
         self.parties = parties
         self.start_date = start_date
+        self.date_column = date_column
 
-        self.data = load_url(url, parties)
+        self.data = load_url(url, parties, date_column=date_column)
 
         date_filter = self.data['Date'] > self.start_date
         self.data = self.data[date_filter]
 
         self._add_statistics()
 
+
+    def add_url(self, url):
+        new_data = load_url(url, self.parties, date_column=self.date_column)
+        self.data = pd.concat([self.data, new_data], sort=True).sort_index(ascending=True)
+
+        date_filter = self.data['Date'] > self.start_date
+        self.data = self.data[date_filter]
+
+        self._add_statistics()
 
     def _add_statistics(self):
         for party in self.parties:
--- loader.py
+++ loader.py
@@ -2,10 +2,20 @@ import pandas as pd
 
 def load_url(url, parties, date_format='%d.%m.%Y', date_column = 'Datum'):
     data = pd.read_html(url)
+    raw_data = data[1]
+    raw_keys = raw_data.keys()
+
+    if date_column not in raw_keys:
+        if 'Datum' in raw_keys:
+            date_column = 'Datum'
+        else:
+            print("Cannot find date_column ", date_column, " in keys: ", raw_keys)
+
+
     polling_data = pd.DataFrame()
-    polling_data['Date'] = pd.to_datetime(data[1][date_column], errors='coerce', format=date_format)
+    polling_data['Date'] = pd.to_datetime(raw_data[date_column], errors='coerce', format=date_format)
     for party in parties:
-        polling_data[party] = pd.to_numeric(data[1][party].apply(lambda x: x.split(' ')[0].replace(',', '.')), errors='coerce')
+        polling_data[party] = pd.to_numeric(raw_data[party].apply(lambda x: x.split(' ')[0].replace(',', '.')), errors='coerce')
 
     polling_data = polling_data.dropna()
 
