dedafef46efe4cb7ee5589b2d2fb0df21f317e4d
Helge <helge.krueger@gmail.com>
2020-01-10 14:30:35 +0100

    Improve api to store credentials

--- apiio/twitter.py
+++ apiio/twitter.py
@@ -2,22 +2,35 @@ import os
 import tweepy
 
 
+
+
 class Twitter:
     def __init__(self):
         self.oauth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
 
+        if 'TWITTER_ACCESS_TOKEN' in os.environ: 
+            self.oauth.access_token = os.environ['TWITTER_ACCESS_TOKEN']
+        if 'TWITTER_ACCESS_TOKEN_SECRET' in os.environ:
+            self.oauth.access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
     def auth(self):
-        redirect_url = self.oauth.get_authorization_url()
+        if not self.oauth.access_token:
+            redirect_url = self.oauth.get_authorization_url()
 
-        print("Please open the following url in a webbrowser and allow access")
-        print(redirect_url)
+            print("Please open the following url in a webbrowser and allow access")
+            print(redirect_url)
 
-        verify = input("Please enter the token: ")
+            verify = input("Please enter the token: ")
 
-        self.oauth.get_access_token(verify)
+            print(self.oauth.get_access_token(verify))
 
         self.api = tweepy.API(self.oauth)
 
-    def post_file(self, filename):
-        self.api.update_with_media(filename)
+    def post_file(self, filename, status=None):
+        if status:
+            self.api.update_with_media(filename, status)
+        else:
+            self.api.update_with_media(filename)
+
+    def timeline(self, username):
+        return self.api.user_timeline(username)
