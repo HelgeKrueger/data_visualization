4c4f3632386cb75a57a2df1aa2173feea7389f79
Helge <helge.krueger@gmail.com>
2020-01-23 16:53:45 +0100

    fix test

--- apiio/test_status_list.py
+++ apiio/test_status_list.py
@@ -1,6 +1,10 @@
+import pytest
+import os
+
 from .status_list import StatusList
 
 
+@pytest.mark.skipif(not os.path.exists('data/wahlrecht_twitter.csv'), reason='requires data file')
 def test_date_format():
     status_list = StatusList()
 
