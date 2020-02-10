9deb9b09792250cc46ff840b89e4f3492549b739
Helge <helge.krueger@gmail.com>
2020-02-10 12:55:56 +0100

    Add thuringia to check_updates

--- check_updates.py
+++ check_updates.py
@@ -29,7 +29,7 @@ print('*' * 70)
 
 status_list.save()
 
-for election in ['germany', 'hamburg']:
+for election in ['germany', 'hamburg', 'thuringia']:
     data, tags = get_current_election_for_name(election)
 
     print()
